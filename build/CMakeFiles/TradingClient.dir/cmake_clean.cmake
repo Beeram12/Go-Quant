e24fb691b6f19f4f37a2659c129b72b666adbdba
Beeram_12 <pranithreddy16.beeram@gmail.com>
2024-12-16 02:38:36 +0530

    Added websocket part

---
+++ build/CMakeFiles/TradingClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TradingClient.dir/src/main.cpp.o"
+  "CMakeFiles/TradingClient.dir/src/main.cpp.o.d"
+  "TradingClient"
+  "TradingClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TradingClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TradingClient.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TradingClient.
+# This may be replaced when dependencies are built.
